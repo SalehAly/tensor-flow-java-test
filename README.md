b4ab9d076c9dfb769a9265d4dadd50503b68537b
Aly Saleh <ali.saleh@unsicon.de>
2017-03-23 16:51:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Adapted from:
+https://github.com/tensorflow/tensorflow/tree/master/tensorflow/java
---
+++ src/main/java/TensorFlowTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by aly on 23.03.17.
+ */
+public class TensorFlowTest {
+    public static void main(String [] args){
+
+    }
+}
